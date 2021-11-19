2e78f75282410a89bddb653785e0b0e08987706f
Joshua Pusch <joshua.pusch@outlook.com>
2021-11-19 22:31:37 +0100

    Add to StoryViewerComponent

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { StoryViewerComponent } from './shared/story-viewer/story-viewer.component';
 
 const routes: Routes = [{
   path: '',
   component: HomeComponent
+}, {
+  path: 'stories',
+  component: StoryViewerComponent
 }];
 
 @NgModule({
