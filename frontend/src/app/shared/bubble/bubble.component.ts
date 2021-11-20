7d9b26697ce838f6f9aca2c9bf572b7e61e92e71
NewLordVile <roman.mayr@gmx.net>
2021-11-20 01:54:30 +0100

    add: stories of different departments on home page

--- frontend/src/app/shared/bubble/bubble.component.ts
+++ frontend/src/app/shared/bubble/bubble.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-bubble',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bubble.component.scss']
 })
 export class BubbleComponent implements OnInit {
+  @Input() userId: string;
 
   constructor() { }
 
