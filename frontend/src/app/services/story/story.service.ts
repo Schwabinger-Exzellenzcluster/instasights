dafce88471262d9b8f5a8362bf45e8dc89fab6ca
Joshua Pusch <joshua.pusch@outlook.com>
2021-11-20 19:35:43 +0100

    Use my API

--- frontend/src/app/services/story/story.service.ts
+++ frontend/src/app/services/story/story.service.ts
@@ -10,6 +10,7 @@ export class StoryService {
   public apiUrl = 'http://192.168.2.129:5000/insights';
 
   constructor(public http: HttpClient) {
+    this.apiUrl = "http://localhost:5000/";
   }
 
   getStoryItems() {
