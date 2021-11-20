9e0e58d6a994ccd93bc1c0fd5d239faedf4de59a
Joshua Pusch <joshua.pusch@outlook.com>
2021-11-20 16:55:48 +0100

    Revert "Fix mobile view"
    This reverts commit 7e1fb01d8cd03d2d6830f0c180b84a554fe62bee.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,17 +1,10 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements AfterViewInit {
-  @ViewChild('wrapper') wrapper: ElementRef;
-
+export class AppComponent {
   title = 'frontend';
-
-  public ngAfterViewInit(): void {
-    let height = this.wrapper.nativeElement.offsetHeight;
-    this.wrapper.nativeElement.style.maxWidth = height * 16/9 + "px";
-  }
 }
