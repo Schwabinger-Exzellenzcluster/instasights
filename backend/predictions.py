1ce91a13e7fc69cf7484466241d4e928cbefbf7e
Tom Papke <tom.papke@outlook.de>
2021-11-21 11:12:15 +0100

    Update insights

--- backend/predictions.py
+++ backend/predictions.py
@@ -8,13 +8,13 @@ from kats.models.prophet import ProphetModel, ProphetParams
 
 from insight import Insight
 from insight import UiTextItem
-from util import get_ui_and_voice_text, df_to_dict, VALUE_LIMIT
+from util import get_ui_and_voice_text, df_to_dict, VALUE_LIMIT, to_change_percent
 
 logger = logging.getLogger('PREDICT')
 
 
 def make_prediction_insight(
-        type: str,
+        metric: str,
         category: str,
         item: str,
         steps: int,
@@ -22,10 +22,10 @@ def make_prediction_insight(
         forecast: pd.DataFrame,
 ) -> Insight:
     positive = change_percent >= 0
-    ui_text = [UiTextItem(type.capitalize(), 1),
-               UiTextItem('of ' + category.capitalize(), 0),
+    ui_text = [UiTextItem(metric.capitalize(), 1),
+               ('of ' + category.capitalize()) if category != 'product group' else 'of',
                UiTextItem(item.capitalize(), 1),
-               ('are' if type.endswith('s') else 'is') + ' predicted to',
+               ('are' if metric.endswith('s') else 'is') + ' predicted to',
                UiTextItem('rise', 1) if positive else UiTextItem('fall', 1),
                UiTextItem(str(math.ceil(abs(change_percent))) + '%', 1),
                'within',
@@ -35,7 +35,7 @@ def make_prediction_insight(
 
     # impact depends on change severity
     forecast.rename(columns={'fcst': 'value'}, inplace=True)
-    return Insight(type, change_percent / 10, ui_text, voice_text, df_to_dict(forecast))
+    return Insight(metric, change_percent / 10, ui_text, voice_text, df_to_dict(forecast))
 
 
 def make_stock_alert_insight(store: str, product: str, steps: int, forecast: pd.DataFrame) -> Insight:
@@ -46,17 +46,19 @@ def make_stock_alert_insight(store: str, product: str, steps: int, forecast: pd.
                'is predicted to',
                UiTextItem('run out', 1),
                'within',
-               UiTextItem(str(steps) + 'Days', 0.5),
+               UiTextItem(str(steps) + ' Days', 0.5),
                ]
 
     ui_text, voice_text = get_ui_and_voice_text(ui_text)
     # impact depends on change severity
-    return Insight('stock', -3, ui_text, voice_text, json.loads(forecast.to_json()))
+    return Insight('stock', -3, ui_text, voice_text, df_to_dict(forecast))
 
 
 def get_predictions(df: pd.DataFrame) -> list[Insight]:
     insights = []
     insights += get_typed_predictions(df, 'revenue', 'city', 'city_id')
+    # insights += get_typed_predictions(df, 'sales', 'product', 'product_id')
+    insights += get_typed_predictions(df, 'sales', 'product group', 'hierarchy1_id')
     insights += get_stock_alert_predictions(df)
     return insights
 
@@ -71,7 +73,7 @@ def get_typed_predictions(df: pd.DataFrame, metric: str,  type: str, type_id: st
         steps = 30  # TODO
 
         curr_value, frcst_val, forecast = get_prediction(df_tv, metric, steps)
-        change_percent = ((frcst_val / curr_value) - 1) * 100
+        change_percent = to_change_percent(curr_value, frcst_val)
         insights.append(
             make_prediction_insight(
                 metric,
@@ -93,6 +95,7 @@ def get_stock_alert_predictions(df: pd.DataFrame) -> list[Insight]:
     stores = df['store_id'].drop_duplicates()
 
     # random selection of stores
+    count = 0
     for store in stores[:VALUE_LIMIT]:
         equals_store = df['store_id'] == store
         df_st = df.loc[equals_store]
@@ -115,8 +118,9 @@ def get_stock_alert_predictions(df: pd.DataFrame) -> list[Insight]:
             # Are we running out of stock
             if (frcst_val <= 0):
                 insights.append(make_stock_alert_insight(store, product, steps, forecast))
-                # return for now
-                return insights  # TODO
+                count += 1
+                if count >= 1: # Prevent long running times
+                    return insights
 
     logger.info('Added stock alert predictions')
 
